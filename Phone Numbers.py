d34f162a8ac4ab47cccc5b56721e9378d77c4310
MarkNixon2 <124679999+MarkNixon2@users.noreply.github.com>
2023-02-07 15:28:42 +0000

    Update Phone Numbers.py

--- Phone Numbers.py
+++ Phone Numbers.py
@@ -1,8 +1,8 @@
-x = input("Enter phone number:")
-y = x.replace(" ", "")
-z = y.replace(")", "")
-b = z.replace("-", "")
-a = b.replace("(", "")
+import string
+
+s = input("Enter phone number:")
+a = s.translate(str.maketrans('', '', string.punctuation))
+a = ''.join(a.split())
 line = ("({})-{}-{}".format(a[:3],a[3:6],a[6:]))
 f = open("PhoneNumbers","w")
 f.write(line)
