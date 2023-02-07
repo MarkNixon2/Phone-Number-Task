319614c62bf419955fc7e3e50efb8b9f9b41627b
MarkNixon2 <124679999+MarkNixon2@users.noreply.github.com>
2023-02-07 16:01:58 +0000

    Update Phone Numbers.py

--- Phone Numbers.py
+++ Phone Numbers.py
@@ -1,9 +1,10 @@
 import string
+address_list = open("PhoneNumbers",'r')
 
-s = input("Enter phone number:")
-a = s.translate(str.maketrans('', '', string.punctuation))
-a = ''.join(a.split())
-line = ("({})-{}-{}".format(a[:3],a[3:6],a[6:]))
-f = open("PhoneNumbers","w")
-f.write(line)
-f.write('\n')
+for line in address_list:
+    a = line.translate(str.maketrans('', '', string.punctuation))
+    a = ''.join(a.split())
+    line = ("({})-{}-{}".format(a[:3],a[3:6],a[6:]))
+    f = open("PhoneNumbers","w")
+    f.write(line)
+    f.write('\n')
