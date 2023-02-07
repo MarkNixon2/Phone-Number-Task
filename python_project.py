4e4c8cff20f5575cd5795167a58e28e6eb46f890
tam173 <87090627+tam173@users.noreply.github.com>
2023-02-07 15:26:29 +0000

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -1,7 +1,9 @@
-
 x = input("Enter phone number:")
 y = x.replace(" ", "")
 z = y.replace(")", "")
 a = z.replace("(", "")
 b = a.replace("-", "")
 print(b)
+
+num = "(" + b[0:3] + ")" + " " + b[3:6] + "-" + b[6:10]
+print(num)
